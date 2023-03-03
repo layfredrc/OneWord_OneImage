596859e94242b5f5fafdcf0dfbca88cf630f77c8
Pierre-Louis Sergent <papa.louis59@gmail.com>
2023-03-03 22:39:13 +0100

    Add allowed_hosts *

--- one_word_one_image/src/backend/settings.py
+++ one_word_one_image/src/backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-#6jw)96l+99*v2o*hsh&=o)2vsttnnpj3&b7!c4bgqomedip!d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
