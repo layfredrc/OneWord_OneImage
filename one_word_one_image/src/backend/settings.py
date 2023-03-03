d982c28f4d34a28b593f72e7c07c7ac8f9c27397
Pierre-Louis Sergent <papa.louis59@gmail.com>
2023-03-03 23:00:27 +0100

    CORS origin *

--- one_word_one_image/src/backend/settings.py
+++ one_word_one_image/src/backend/settings.py
@@ -177,7 +177,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = False
  
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000',
-    'http://localhost:3000',
-]
+CORS_ORIGIN_WHITELIST = ["*"]
